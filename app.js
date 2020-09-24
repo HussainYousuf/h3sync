ab8e7fd5ea0ab2c4402fc361ffd22acbe7acdc61
HussainYousuf <hussainyousuf@folio3.com>
2020-09-25 01:35:52 +0500

    progress

--- app.js
+++ app.js
@@ -1,61 +1,12 @@
 let axios = require("axios");
 let fs = require('fs/promises');
+let fsSync = require("fs");
 let path = require("path");
 let FormData = require('form-data');
+let cwait = require("cwait");
+let TaskQueue = cwait.TaskQueue;
 
-let args = process.argv.slice(2);
-
-let filePath = args[0];
-let folderId = args[1] || "-15";
-
-syncFile(path.resolve(__dirname, filePath), folderId);
-
-async function syncFile(filePath, folderId) {
-    try {
-        let stats = await fs.stat(filePath);
-        let fileName = path.basename(filePath);
-        let body = {
-            fileName,
-            folderId,
-        };
-
-        if (stats.isFile()) {
-            body.type = "file";
-            body.extension = path.extname(filePath) || ".txt";
-            body.extension = fileMap[body.extension.substring(1).toLowerCase()] || "PLAINTEXT";
-            if (["CERTIFICATE", "CONFIG", "CSV", "HTMLDOC", "JAVASCRIPT", "JSON", "PLAINTEXT", "SCSS", "STYLESHEET", "XMLDOC"].includes(body.extension))
-                body.contents = await fs.readFile(filePath, { encoding: 'utf-8' });
-            else
-                body.contents = await fs.readFile(filePath, { encoding: 'base64' });
-            let result = await request("https://tstdrv1796095.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=1002&deploy=1&compid=TSTDRV1796095&h=3c0a9cc1d54b2f3df341", body);
-            if (result.id) {
-                console.log(fileName + " synced in folder whose id is: " + folderId);
-            }
-        } else if (stats.isDirectory()) {
-            body.type = "folder";
-            let result = await request("https://tstdrv1796095.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=1002&deploy=1&compid=TSTDRV1796095&h=3c0a9cc1d54b2f3df341", body);
-            if (result.id) {
-                console.log(fileName + " has folder id: " + result.id);
-                let files = await fs.readdir(filePath);
-                for (let file of files) syncFile(path.resolve(filePath, file), result.id);
-            }
-        }
-
-    } catch (error) {
-        console.log(error);
-        if (body) console.log(body.fileName + " failed to sync");
-    }
-}
-
-async function request(url, body) {
-    let form = new FormData();
-    form.append('body', JSON.stringify(body));
-    let headers = Object.assign({ "User-Agent": "Mozilla/5.0" }, form.getHeaders());
-    let result = await axios.post(url, form, { headers });
-    return result.data;
-}
-
-var fileMap = {};
+let fileMap = {};
 fileMap.appcache = "APPCACHE";
 fileMap.dwf = "AUTOCAD";
 fileMap.dwg = "AUTOCAD";
@@ -121,5 +72,132 @@ fileMap.doc = fileMap.docx = fileMap.dot = "WORD";
 fileMap.xml = "XMLDOC";
 fileMap.xsd = "XSD";
 fileMap.zip = fileMap.lzh = fileMap.lha = "ZIP";
+
+let queue = new TaskQueue(Promise, 25);
+
+let IGNORE = ".syncignore";
+let CONFIG = ".sync";
+let URL = "";
+
+function cli() {
+    let args = process.argv.slice(2);
+    if (args[0] == "init") init(args[1] || "");
+    else if (args[0] == "add") add(args[1] || ".", args[2] || "-15");
+    else if (args[0] == "sync") sync();
+    else help();
+}
+
+function init(url) {
+    if (url) URL = url;
+    else help();
+}
+
+function help() {
+    console.log("help");
+}
+
+async function add(file, parent) {
+    let obj = {};
+    let ignore = [];
+    let ignoreFile;
+    if (fsSync.existsSync(IGNORE)) ignoreFile = fsSync.readFileSync(IGNORE, { encoding: "utf-8" });
+    if (ignoreFile) ignoreFile.split("\n").forEach(line => ignore.push(path.resolve(__dirname, line)));
+    if (fsSync.existsSync(CONFIG)) obj = JSON.parse(fsSync.readFileSync(CONFIG));
+    await _add(path.resolve(__dirname, file), parent);
+    fsSync.writeFileSync(CONFIG, JSON.stringify(obj));
+
+    async function _add(filePath, parent) {
+        let stats = await fs.stat(filePath);
+        let body = {
+            name: path.basename(filePath),
+            parent
+        };
+        if (stats.isDirectory()) {
+            if (ignore.includes(filePath)) return;
+            if (!obj[filePath]) {
+                body.type = "folder";
+                let result = await request(URL, body);
+                if (result.id) {
+                    console.log(`${filePath} synced`);
+                    body.id = result.id;
+                    obj[filePath] = body;
+                } else {
+                    console.log(`unable to sync ${filePath}`);
+                }
+            }
+            let files = await fs.readdir(filePath);
+            let promises = [];
+            files.forEach(file => promises.push(file));
+            await Promise.all(promises.map(queue.wrap(file => _add(path.resolve(filePath, file), obj[filePath].id))));
+        } else {
+            if (ignore.includes(filePath)) return;
+            if (!obj[filePath] || stats.mtimeMs > obj[filePath].mtimeMs ) {
+                body.type = "file";
+                body.extension = fileMap[path.extname(filePath).substring(1).toLowerCase()] || "PLAINTEXT";
+                
+                body.mtimeMs = 0;
+                obj[filePath] = body;
+            }
+        }
+    }
+}
+
+async function sync() {
+    let obj;
+    let ignore = [];
+    let ignoreFile;
+    if (fsSync.existsSync(IGNORE)) ignoreFile = fsSync.readFileSync(IGNORE, { encoding: "utf-8" });
+    if (ignoreFile) ignoreFile.split("\n").forEach(line => ignore.push(path.resolve(__dirname, line)));
+    if (fsSync.existsSync(CONFIG)) obj = JSON.parse(fsSync.readFileSync(CONFIG));
+    else {
+        help();
+        return;
+    }
+    let promises = [];
+    for (let filePath in obj) {
+        let file = Object.assign({ filePath }, obj[filePath]);
+        if (fsSync.existsSync(filePath) && !ignore.includes(filePath)) {
+            if (file.type == "file" && fsSync.statSync(filePath).mtimeMs > file.mtimeMs) {
+                if (["CERTIFICATE", "CONFIG", "CSV", "HTMLDOC", "JAVASCRIPT", "JSON", "PLAINTEXT", "SCSS", "STYLESHEET", "XMLDOC"].includes(file.extension))
+                    file.contents = fsSync.readFileSync(filePath, { encoding: 'utf-8' });
+                else
+                    file.contents = fsSync.readFileSync(filePath, { encoding: 'base64' });
+                promises.push(file);
+            }
+        } else {
+            if (file.id) {
+                file.type = "delete";
+                promises.push(file);
+            }
+            else delete file;
+        }
+    }
+    let result = await Promise.all(promises.map(queue.wrap(async file => await request(URL, file))));
+    for (let file of result) {
+        if (!file) continue;
+        if (file.type == "file") {
+            console.log(`${file.filePath} created and has id ${file.id}`);
+            obj[file.filePath].id = file.id;
+            obj[file.filePath].mtimeMs = fsSync.statSync(file.filePath).mtimeMs;
+        } else if (file.type == "delete") {
+            delete obj[file.filePath];
+            console.log(`${file.filePath} has been deleted in netsuite`);
+        }
+    }
+    fsSync.writeFileSync(CONFIG, JSON.stringify(obj));
+}
+
+async function request(url, body) {
+    try {
+        let form = new FormData();
+        form.append('body', JSON.stringify(body));
+        let headers = Object.assign({ "User-Agent": "Mozilla/5.0" }, form.getHeaders());
+        let result = await axios.post(url, form, { headers });
+        return result.data;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 
 
